9f5038e0e4007faaa24e9cc45150b396d715160e
sulemc <susanna.mcdo@gmail.com>
2018-06-11 12:46:42 -0500

    broken links

--- main.js
+++ main.js
@@ -4,44 +4,92 @@ const url1 = ' https://cors-anywhere.herokuapp.com/http://www.rayheberer.ai/json
 const url2 = 'https://cors-anywhere.herokuapp.com/https://goofy-poitras-4bf646.netlify.com/presentation_data.json';
 
 const url3 = 'https://cors-anywhere.herokuapp.com/https://stoic-mccarthy-ac09cf.netlify.com/presentation_data.json';
+
+
+
+let num = 0;
 function getData(url) {
     axios.get(url)
     .then(function (response) {
         let you = response.data;
+        num = num+1;
         itterateObject(you);
     })
     .catch(function (error) {
-        person.innerHTML = "(An error has occurred.)";
+        document.innerHTML = "(An error has occurred.)";
 
     }); 
 }
-
+let title = '';
 function itterateObject (data) {
     for (var property in data){ 
         if (typeof data[property] === 'object'){
             let blog = data[property];
             for (var property2 in blog){
-               addElement (blog[property2]);
+                if (property2 === 'title'){
+                    title = blog[property2];
+                }
+                if (property2 === 'url'){
+                    console.log(blog[property2])
+                    addBlogLinkElement(blog[property2], title);
+                }
+                else {
+               //addBlogElement (blog[property2]);
+                }
             }
         }
         else{
-        addElement(data[property]);
+        if (property === 'name'){
+        addNameElement(data[property]);}
+        if (property === 'netlify_subdomain'){
+            addLinkElement(data[property]);
+        }
         }
     }
-        return string1;
+
 }
 
-function addElement (info) { 
-    var newDiv = document.createElement("ul"); 
+function addNameElement (info) { 
+    var newDiv = document.createElement(`div-name`); 
     var newContent = document.createTextNode(info); 
     // add the text node to the newly created div
     newDiv.appendChild(newContent);  
-  
-    // add the newly created element and its content into the DOM 
     var currentDiv = document.getElementById("div1"); 
     document.body.insertBefore(newDiv, currentDiv); 
   }
 
-getData(url1); 
+  function addLinkElement (info) { 
+
+    var newDiv = document.createElement("a"); 
+    newDiv.href = info;
+    var newContent = document.createTextNode(info) 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+    var currentDiv = document.getElementById("div1"); 
+    document.body.insertBefore(newDiv, currentDiv); 
+  }
+
+  function addBlogElement (info) { 
+    var newDiv = document.createElement(`ul`); 
+    var newContent = document.createTextNode(info); 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+    var currentDiv = document.getElementById("div1"); 
+    document.body.insertBefore(newDiv, currentDiv); 
+  }
+
+  function addBLogLinkElement (info, articleTitle) { 
+    console.log('hello')
+    var newDiv = document.createElement("a"); 
+    newDiv.href = info;
+    var newContent = document.createTextNode(articleTitle) 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+    var currentDiv = document.getElementById("div1"); 
+    document.body.insertBefore(newDiv, currentDiv); 
+  }
+
+
+//getData(url1); 
 getData(url2);
-getData(url3);
+//getData(url3);
